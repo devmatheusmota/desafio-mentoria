2bc669a0e76df58cf22b02e23d3de1f2aab488a0
Matheus Mota <devmatheusmota@gmail.com>
2022-10-28 07:50:44 -0300

    Changed server port

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,6 @@ app.use(express.json());
 
 app.use('/', router);
 
-app.listen(3000, () => {
-	console.log('Listening on http://localhost:3000');
+app.listen(3333, () => {
+	console.log('Listening on http://localhost:3333');
 });
